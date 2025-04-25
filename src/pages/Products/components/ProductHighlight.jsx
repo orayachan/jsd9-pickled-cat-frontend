5c6796b199b7595ccdf849e0f414af191d591316
Oraya Chantanawat <ochantanawat@gmail.com>
2025-04-26 06:08:40 +0700

    feat(ui): add image and text layout

--- src/pages/Products/components/ProductHighlight.jsx
+++ src/pages/Products/components/ProductHighlight.jsx
@@ -12,24 +12,25 @@ export function ProductHighlight() {
     <div className='flex flex-col content-center-safe justify-center mx-auto max-w-screen-2xl'>
       <h2
         id='highlight'
-        className='text-primary-700 font-bold text-2xl py-4'
+        className='text-primary-700 text-center font-bold text-2xl py-4'
       >
         สินค้าขายดี
       </h2>
       <section className='px-8'>
         <Carousel className='w-full'>
           <CarouselContent className='-ml-1'>
-            {Array.from({ length: 4 }).map((_, index) => (
+            {Array.from({ length: 6 }).map((_, index) => (
               <CarouselItem
                 key={index}
                 className='pl-1 md:basis-1/2 lg:basis-1/3'
               >
                 <div className='p-1'>
                   <Card>
-                    <CardContent className='flex aspect-square items-center justify-center p-6'>
-                      <span className='text-2xl font-semibold'>
+                    <CardContent className='flex flex-col aspect-square items-center justify-center p-6 h-80 bg-primary-50'>
+                      <img src={`@/error/graycat404.png`} alt='' className='object-cover w-32 h-32 sm:w-40 sm:h-40' />
+                      <p className='text-lg'>
                         {index + 1}
-                      </span>
+                      </p>
                     </CardContent>
                   </Card>
                 </div>
