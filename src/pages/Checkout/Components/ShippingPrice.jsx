e87af50f6206e348b146d2abba955c86c3e547b5
Oraya Chantanawat <ochantanawat@gmail.com>
2025-04-28 01:43:56 +0700

    feat(ui): add ShippingPrice component

---
+++ src/pages/Checkout/Components/ShippingPrice.jsx
@@ -0,0 +1,36 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+export function ShippingPrice() {
+  return (
+    <Card className='min-w-[350px] px-4'>
+      <CardHeader>
+        <CardTitle>ค่าบริการจัดส่ง</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CardDescription className='mb-2 flex items-start justify-between gap-4'>
+          <span>ระยะทางไม่เกิน 5 กิโลเมตร</span>
+          <span>฿ xxx.xx</span>
+        </CardDescription>
+        <CardDescription className='mb-2 flex items-start justify-between gap-4'>
+          <span>ระยะทางไม่เกิน 10 กิโลเมตร</span>
+          <span>฿ xxx.xx</span>
+        </CardDescription>
+        <CardDescription className='mb-2 flex items-start justify-between gap-4'>
+          <span>ระยะทางไม่เกิน 20 กิโลเมตร</span>
+          <span>฿ xxx.xx</span>
+        </CardDescription>
+        <CardDescription className='mb-2 flex items-start justify-between gap-4'>
+          <span>ระยะทางมากกว่า 20 กิโลเมตรขึ้นไป</span>
+          <span>฿ xxx.xx</span>
+        </CardDescription>
+      </CardContent>
+    </Card>
+  );
+}
