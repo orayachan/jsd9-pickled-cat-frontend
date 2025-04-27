4daf676c119eb47df1bf346fb73823d82df64d02
Oraya Chantanawat <ochantanawat@gmail.com>
2025-04-28 03:56:02 +0700

    feat(ui): add Itemcard component for checkout page

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import { Link } from 'react-router';
+import { BcrumbCheckout } from './Components/BcrumbCheckout';
+import { Cart } from './Components/Cart';
+import { OrderSummary } from './Components/OrderSummary';
+import { ShippingPrice } from './Components/ShippingPrice';
 
 export function Checkout() {
   return (
-    <div>Checkout</div>
-  )
+    <div className='bg-secondary-50 min-w-dvw px-8 pt-4 pb-16'>
+      <main className='mx-auto flex max-w-screen-2xl flex-col justify-center'>
+        {/* Breadcrumb */}
+        <BcrumbCheckout />
+        <section className='flex flex-col items-start justify-center gap-4 md:flex-row'>
+          {/* Cart */}
+          <Cart />
+          <aside className='flex flex-col items-center justify-center'>
+            {/* Order summary */}
+            <OrderSummary />
+            <p className='text-primary-700 py-4'>
+              <Link to='Login' className='hover:text-primary-500 font-semibold'>
+                เข้าสู่ระบบ
+              </Link>
+              <span> หรือ </span>
+              <Link
+                to='Registration'
+                className='hover:text-primary-500 font-semibold'
+              >
+                สมัครสมาชิก
+              </Link>
+            </p>
+            {/* Shipping Price */}
+            <ShippingPrice />
+          </aside>
+        </section>
+      </main>
+    </div>
+  );
 }
---
+++ src/pages/Checkout/Components/ItemCard.jsx
@@ -0,0 +1,27 @@
+import { Checkbox } from '@/components/ui/checkbox';
+import { HiOutlineTrash } from 'react-icons/hi';
+import MockPhoto from '/public/error/mini-graycat.png';
+
+export function ItemCard() {
+  return (
+    <section className='flex items-center-safe justify-start pb-4'>
+      <Checkbox className='mx-2' />
+      <figure className='border-secondary-50 mx-2 h-[120px] w-[120px] rounded-md border-1'>
+        <img src={MockPhoto} alt='product image' className='fill' />
+      </figure>
+      <div>
+        <p className='text-primary-700 font-semibold'>ชื่อสินค้า</p>
+        <p className='text-primary-700 text-sm'>ตัวเลือก, ขนาด</p>
+        <p className='text-primary-700 font-semibold'>฿ xxx.xx</p>
+        <div className='mt-2 flex items-center justify-center gap-2'>
+          <div className='flex justify-around items-center bg-primary-50 w-36 rounded-full py-1 mt-2'>
+            <button className='font-bold hover:text-primary-500'>-</button>
+            <button className='font-bold hover:text-primary-500'>1</button>
+            <button className='font-bold hover:text-primary-500'>+</button>
+          </div>
+          <HiOutlineTrash className='text-primary-500 hover:text-primary-800' />
+        </div>
+      </div>
+    </section>
+  );
+}
