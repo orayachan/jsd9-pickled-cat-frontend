0f78194c6d7b3264994577cf955780f0c2aa4193
Oraya Chantanawat <ochantanawat@gmail.com>
2025-05-02 17:53:37 +0700

    fix(ui): adjust image paths

--- src/pages/Checkout/Components/ItemCard.jsx
+++ src/pages/Checkout/Components/ItemCard.jsx
@@ -1,13 +1,12 @@
 import { Checkbox } from '@/components/ui/checkbox';
 import { HiOutlineTrash } from 'react-icons/hi';
-import MockPhoto from '/public/error/mini-graycat.png';
 
 export function ItemCard() {
   return (
     <section className='flex items-center-safe justify-start pb-4'>
       <Checkbox className='mx-2' />
       <figure className='border-secondary-50 mx-2 h-[120px] w-[120px] rounded-md border-1'>
-        <img src={MockPhoto} alt='product image' className='fill' />
+        <img src='https://img2.pic.in.th/pic/mini-graycat.png' alt='product image' className='fill' />
       </figure>
       <div>
         <p className='text-primary-700 font-semibold'>ชื่อสินค้า</p>
