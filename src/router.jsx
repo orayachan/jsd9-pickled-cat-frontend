be341a3bb62e2474570abb4d345ef57be5a0033d
GunDeTama <bleed99e@gmail.com>
2025-05-04 19:48:24 +0700

    feat(path): edit admin dashboard path

--- src/router.jsx
+++ src/router.jsx
@@ -5,7 +5,7 @@ import {
   Error404,
   Minimal,
 } from '@/components/layouts';
-import { Checkout, Home, Login, Payment, Register } from '@/pages';
+import { Checkout, Home, Login, MyAccount, Payment, Register } from '@/pages';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 
 const router = createBrowserRouter([
@@ -25,28 +25,17 @@ const router = createBrowserRouter([
     children: [
       { path: 'login', Component: Login },
       { path: 'register', Component: Register },
-      { path: '*', Component: Error404 },
-      { path: 'dashboard', Component: Dashboard },
+      { path: '404', Component: Error404 },
+      { path: 'admin/dashboard', Component: AdminDashboard },
     ],
   },
-  // {
-  //   path: 'dashboard',
-  //   Component: Dashboard,
-  //   children: [
-  //     { index: true, Component: MyAccount },
-  //     // TODO: Modify to redirect to `/dashboard`, not just show something else.
-  //     { path: '*', Component: MyAccount },
-  //   ],
-  // },
   {
-    path: 'admin/dashboard',
-    Component: AdminDashboard,
-  },
-  // TODO: Make other non-existed paths redirect to this.
-  {
-    path: '404',
-    Component: Minimal,
-    children: [{ index: true, Component: Error404 }],
+    path: 'dashboard',
+    Component: Dashboard,
+    children: [
+      { index: true, Component: MyAccount },
+      { path: '*', Component: MyAccount },
+    ],
   },
 ]);
 
