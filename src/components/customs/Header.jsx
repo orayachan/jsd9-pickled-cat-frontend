bef6dbcb4ba97f7809d6bf63821076df219f5043
Oraya Chantanawat <ochantanawat@gmail.com>
2025-05-13 15:22:08 +0700

    fix(ui): adjust header styles

--- src/components/customs/Header.jsx
+++ src/components/customs/Header.jsx
@@ -13,44 +13,44 @@ export function Header() {
     <header className='content-center-safe border-b bg-white px-8 py-4 text-center'>
       <nav className='mx-auto flex items-center justify-between gap-4 lg:max-w-screen-2xl'>
         {/* Part - start */}
-        <h1 className='flex items-start text-xl font-bold italic lg:text-2xl'>
-          <Link to='/' className='hover:text-primary-700'>
+        <h1 className='flex items-start text-xl font-bold italic lg:text-3xl'>
+          <Link to='/' className='hover:text-primary-600'>
             PICKLED-CAT
           </Link>
         </h1>
         {/* Part - middle */}
-        <ul className='hidden items-center justify-center gap-4 font-medium lg:flex'>
+        <ul className='py-4 px-8 bg-primary-50 rounded-full hidden items-center justify-center font-medium xl:flex'>
           <li>
-            <Link to='/' className='hover:text-primary-600'>
+            <Link to='/' className='hover:text-primary-600 mx-6'>
               หน้าแรก
             </Link>
           </li>
           <li>
-            <a href='#products' className='hover:text-primary-600'>
+            <a href='#products' className='hover:text-primary-600 mx-6'>
               สินค้าของเรา
             </a>
           </li>
           <li className='hidden'>
-            <a href='#highlight' className='hover:text-primary-600'>
+            <a href='#highlight' className='hover:text-primary-600 mx-6'>
               สินค้าขายดี
             </a>
           </li>
           <li>
-            <a href='#faqs' className='hover:text-primary-600'>
+            <a href='#faqs' className='hover:text-primary-600 mx-6'>
               คำถามที่พบบ่อย
             </a>
           </li>
           <li>
-            <a href='#contact' className='hover:text-primary-600'>
+            <a href='#contact' className='hover:text-primary-600 mx-6'>
               ติดต่อเรา
             </a>
           </li>
         </ul>
         {/* Part - end */}
-        <div className='flex items-center justify-end gap-4'>
+        <div className='flex items-center justify-end gap-4 text-lg md:text-xl'>
           <Link
             to='/Checkout'
-            className='hover:text-primary-600 flex items-center justify-center rounded-full lg:h-[50px] lg:w-[50px] lg:bg-[#f5f1ee] lg:text-[#875541]'
+            className='hover:text-primary-600 flex items-center justify-center rounded-full lg:h-[50px] lg:w-[50px] lg:bg-[#f5f1ee] lg:text-primary-500'
           >
             <HiOutlineShoppingCart />
           </Link>
@@ -69,20 +69,20 @@ export function Header() {
               <HiOutlineMenu />
             </button>
             {isMenuOpen && (
-              <ul className='absolute right-0 z-50 mt-4 w-36 rounded-lg border bg-white shadow-md'>
-                <li className='hover:text-primary-600 mr-4 text-end'>
+              <ul className='absolute right-0 z-50 mt-4 w-44 rounded-lg border bg-white shadow-md'>
+                <li className='text-lg hover:text-primary-600 m-4 text-end'>
                   <Link to='/Dashboard'>ข้อมูลของฉัน</Link>
                 </li>
-                <li className='hover:text-primary-600 mr-4 text-end'>
+                <li className='text-lg hover:text-primary-600 m-4 text-end'>
                   <Link to='/Dashboard'>คำสั่งซื้อ</Link>
                 </li>
-                <li className='hover:text-primary-600 mr-4 text-end'>
+                <li className='text-lg hover:text-primary-600 m-4 text-end'>
                   <Link to='/Dashboard'>รายการยกเลิก</Link>
                 </li>
-                <li className='hover:text-primary-600 mr-4 text-end'>
+                <li className='text-lg hover:text-primary-600 m-4 text-end'>
                   <Link to='/Dashboard'>รีวิวสินค้า</Link>
                 </li>
-                <li className='hover:text-primary-600 mr-4 text-end'>
+                <li className='text-lg hover:text-primary-600 m-4 text-end'>
                   <Link to='/'>ออกจากระบบ</Link>
                 </li>
               </ul>
@@ -91,7 +91,7 @@ export function Header() {
           <button className='hover:text-primary-600 hidden items-center justify-center rounded-full lg:h-[50px] lg:w-[50px] lg:bg-yellow-100'>
             <HiOutlineUserCircle />
           </button>
-          <p className='hidden flex-col items-start justify-center lg:flex'>
+          <p className='hidden flex-col items-start justify-center text-lg lg:flex'>
             <span className='font-semibold'>สวัสดี</span>
             <Link to='/Login' className='hover:text-primary-600'>
               เข้าสู่ระบบ
