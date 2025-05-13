df56a31f00aff8610ee5cf857103bb01803ad117
Oraya Chantanawat <ochantanawat@gmail.com>
2025-05-13 12:08:19 +0700

    fix(ui): change color styles on product detail page

--- src/pages/Product/ProductDetail.jsx
+++ src/pages/Product/ProductDetail.jsx
@@ -94,9 +94,9 @@ export const ProductDetail = () => {
   return (
     <section className='mx-auto max-w-7xl p-4'>
       {/* Breadcrumb */}
-      <nav className='text-sm text-gray-500'>
+      <nav className='text-sm text-primary-600'>
         <Link to='/'>Home</Link> &gt;{' '}
-        <span className='text-gray-700'>{product.name}</span>
+        <span className='text-primary-900'>{product.name}</span>
       </nav>
 
       <div className='mt-4 flex flex-col lg:flex-row lg:gap-8'>
@@ -105,7 +105,7 @@ export const ProductDetail = () => {
 
         {/* Product Details */}
         <article className='mt-6 lg:mt-0 lg:w-1/2'>
-          <h1 className='text-2xl font-semibold'>{product.name}</h1>
+          <h1 className='text-2xl text-primary-800 font-semibold'>{product.name}</h1>
 
           <div className='flex items-center space-x-2 text-lg text-yellow-400'>
             <p>★★★★★</p>
@@ -114,7 +114,7 @@ export const ProductDetail = () => {
 
           {/* Price */}
           <div className='mt-3 flex items-center space-x-3'>
-            <span className='text-3xl font-bold text-gray-900'>
+            <span className='text-3xl font-bold text-primary-900'>
               ฿{product.price}
             </span>
 
@@ -135,13 +135,13 @@ export const ProductDetail = () => {
           </div>
 
           {/* Description */}
-          <p className='mt-3 leading-relaxed text-gray-600'>
+          <p className='mt-3 leading-relaxed text-primary-600'>
             {product.description}
           </p>
 
           {/* Food Recipes */}
           <div className='mt-5'>
-            <h2 className='font-semibold text-gray-700'>เลือกที่ลูกชอบ</h2>
+            <h2 className='font-semibold text-primary-700'>เลือกที่ลูกชอบ</h2>
             <div className='mt-2 grid grid-cols-3 gap-2'>
               {product.option.map((opt) => (
                 <button
@@ -149,8 +149,8 @@ export const ProductDetail = () => {
                   onClick={() => setSelectedOption(opt)}
                   className={`rounded-lg border bg-gray-200 px-4 py-2 ${
                     selectedOption === opt ?
-                      'bg-black text-white'
-                    : 'bg-gray-200 hover:bg-gray-300'
+                      'bg-primary-800 text-white'
+                    : 'bg-primary-200 hover:bg-primary-300'
                   }`}
                 >
                   {opt}
@@ -161,7 +161,7 @@ export const ProductDetail = () => {
 
           {/* Packaging Size */}
           <div className='mt-5'>
-            <h2 className='font-semibold text-gray-700'>เลือกขนาดที่ต้องการ</h2>
+            <h2 className='font-semibold text-primary-700'>เลือกขนาดที่ต้องการ</h2>
             <div className='mt-2 flex space-x-3'>
               {product.sizes.map((size) => (
                 <button
@@ -169,8 +169,8 @@ export const ProductDetail = () => {
                   onClick={() => setSelectedSize(size)}
                   className={`rounded-lg border px-5 py-3 text-gray-700 transition ${
                     selectedSize === size ?
-                      'bg-black text-white'
-                    : 'text-gray-700 hover:bg-black hover:text-white'
+                      'bg-primary-800 text-white'
+                    : 'text-primary-700 hover:bg-primary-300 hover:text-white'
                   }`}
                 >
                   {size}
@@ -198,7 +198,7 @@ export const ProductDetail = () => {
             </div>
             <button
               onClick={handleAddToCart}
-              className='w-full rounded-full bg-black px-6 py-4 text-lg font-semibold text-white transition hover:bg-gray-800'
+              className='w-full rounded-full bg-primary-800 px-6 py-4 text-lg font-semibold text-white transition hover:bg-primary-700'
             >
               Add to Cart
             </button>
