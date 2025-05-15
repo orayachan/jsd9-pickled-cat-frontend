53465545ace86f3acdd03d7427f73e30a664b3df
KUNG PIYAPHON <kungpiyaporn.0452@gmail.com>
2025-05-15 18:56:17 +0700

    feat(payment): updated submit payments alert success

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,13 +1,15 @@
 import { Button } from '@/components/ui/button';
-import axios from 'axios';
 import { useState } from 'react';
+import { useNavigate } from 'react-router';
 import { BcrumbPayment } from './Components/BcrumbPayment';
 import { PaymentMethod } from './Components/PaymentMethod';
 import { PaymentSummary } from './Components/PaymentSummary';
 import { ShippingForm } from './Components/ShippingForm';
 
 export function Payment() {
-  const [formData, setFormData] = useState({
+  const navigate = useNavigate();
+
+  const initialFormData = {
     firstName: '',
     lastName: '',
     phone: '',
@@ -16,49 +18,27 @@ export function Payment() {
     subDistrict: '',
     province: '',
     postalCode: '',
-  });
-
+  };
+  const [formData, setFormData] = useState(initialFormData);
   const [paymentMethod, setPaymentMethod] = useState('bankTransfer');
+
+   const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const handlePaymentMethodChange = (e) => {
     setPaymentMethod(e.target.value);
   };
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    // Backend (TBC)
+    localStorage.removeItem("cart");
 
-    let paidAt = new Date().toISOString(); // Get the current date and time in ISO format
-    let updateAt = new Date().toISOString(); // Get the current date and time in ISO format
+    setFormData(initialFormData);
+    setPaymentMethod("banktransfer");
 
-    let status = 'Pending'; // Set the initial status to 'Pending'
-
-    if (paymentMethod === 'cod') {
-      // Handle cash on delivery
-      status = 'Paid'; // Set the status to 'Paid'
-      paidAt = new Date().toISOString(); // Set the paidAt date to now
-      updateAt = new Date().toISOString(); // Set the updateAt date to now
-    }
-
-    try {
-      const response = await axios.post(
-        'http://localhost:3000/api/payments', // Replace with your backend URL
-        {
-          payment_method: paymentMethod,
-          amount: 100, // Replace with the actual amount
-          payment_status: status,
-          payment_date: paidAt,
-          update_at: updateAt,
-        },
-      );
-      console.log('Payment successful:', response.data);
-    } catch (error) {
-      console.error('Error processing payment:', error);
-    }
+    alert("คำสั่งซื้อของคุณได้รับการยืนยันแล้ว!");
+    navigate("/");
   };
 
   return (
@@ -69,7 +49,7 @@ export function Payment() {
           onSubmit={handleSubmit}
           className='mt-8 grid grid-cols-1 gap-8 lg:grid-cols-3'
         >
-          {/* PART: Left */}
+          {/* Left Side */}
           <div className='space-y-8 lg:col-span-2'>
             <ShippingForm formData={formData} handleChange={handleChange} />
             <PaymentMethod
@@ -80,10 +60,11 @@ export function Payment() {
               type='submit'
               className='bg-primary-600 hover:bg-primary-700 w-full rounded-full py-3 transition'
             >
-              ยืนยันการสังซื้อ
+              ยืนยันการสั่งซื้อ
             </Button>
           </div>
-          {/* PART: Right */}
+
+          {/* Right Side */}
           <aside className='sticky top-24'>
             <PaymentSummary />
           </aside>
