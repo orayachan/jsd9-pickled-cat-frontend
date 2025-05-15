99f925377eeda5ea3bd659aab16da6d035b030ba
kungpiyaphon <93111308+kungpiyaphon@users.noreply.github.com>
2025-05-15 12:16:08 +0000

    style: auto-format, lint, and test build script

--- src/services/ordersService.js
+++ src/services/ordersService.js
@@ -1,6 +1,6 @@
-import api from "./api";
+import api from './api';
 
 export const createOrder = async (orderData) => {
-  const response = await api.post("/api/orders/my",orderData);
+  const response = await api.post('/api/orders/my', orderData);
   return response.data;
 };
